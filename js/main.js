f4c03ac4448b07285ef8377d72b3c1863f010910
Stephen Young <stephenjayyoung@gmail.com>
2020-04-20 21:11:16 -0700

    added json file from my server, added in new data fields

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ var btn = document.getElementById("btn");
 btn.addEventListener("click", function() {
 	
 	var ourRequest = new XMLHttpRequest();
-	ourRequest.open('GET', "http://www.filltext.com/?rows=5&fname={firstName}&lname={lastName}&city={city}&pretty=true");
+	ourRequest.open('GET', "http://youngcreativespace.com/bands.json");
 	ourRequest.onload = function() {
 		var ourData = JSON.parse(ourRequest.responseText);
 		renderHTML(ourData);
@@ -18,12 +18,13 @@ function renderHTML(data) {
 	for (i=0; i < data.length; i++){
 		htmlString += 
 		"<div class='card'>"
-		 	+ "<img class='card-img-top' src='https://images.unsplash.com/photo-1561617587-d8f3586a1d57?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=967&q=80'>"  
+		 	+ "<img class='card-img-top' src=" + data[i].image + ">"  
 		 	+ "<div class='card-body'>"
-			 	+ "<h5 class='card-title'>" + data[i].fname + " " + data[i].lname + "</h5>"
+			 	+ "<h5 class='card-title'>" + data[i].band_name + "</h5>"
 			 	+ "<span class='card-text'>" 
-				 	+ "<p><strong>Genre</strong>: add genre here" + "</p>"
+				 	+ "<p><strong>Genre</strong>: " + data[i].genre + "</p>"
 				 	+ "<p><strong>City</strong>: " + data[i].city + "</p>"
+					+ "<a><strong>Link</strong>: " + data[i].insta_link + "</a>"
 			 	+ "</span>" 
 		 	+ "</div>"
 		+ "</div>"
